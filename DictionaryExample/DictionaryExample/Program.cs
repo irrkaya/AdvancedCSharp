f6b47d32632865b9457d2d01709b47a9baac61e6
Irem Soyaker <irem_95@windowslive.com>
2023-02-22 23:01:38 +1100

    Add files via upload

---
+++ DictionaryExample/DictionaryExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DictionaryExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //create an empty dictionary
+            Dictionary<int, string> employees = new Dictionary<int, string>()
+            {
+                { 101,"Scott"},
+                { 102, "Smith"},
+                { 103,"Allen"}
+            };
+
+            //Remove
+            employees.Remove(102);
+
+            // foreach loop for dictionary
+            foreach (KeyValuePair<int, string> item in employees)
+            {
+                Console.WriteLine(item.Key + "," + item.Value);
+            }
+
+            //get value based on the key
+            string s = employees[101];
+            Console.WriteLine("\nValue at 101: " + s);
+
+            //Keys
+            Dictionary<int, string>.KeyCollection keys = employees.Keys;
+            Console.WriteLine("\nKeys:");
+            foreach(int item in keys )
+            {
+                Console.WriteLine(item);
+            }
+
+            //Duplicate key:exception
+            //employees.Add(104, "Scott"); //Exception
+
+            //ContainsKey
+            bool a = employees.ContainsKey(103);
+            Console.WriteLine("ContainsKey: " +a);
+
+            //ContainsValue
+            bool b = employees.ContainsValue("Scott");
+            Console.WriteLine("ContainsValue: " +b);
+
+            Console.ReadKey();
+        }
+    }
+}
